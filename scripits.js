ffed246adfc62115d639b733cf1a2ac28e504f72
rafaelscibor-design <rafael.scibor@escola.pr.gov.br>
2026-09-21 08:11:34 -0300

    abra cadrabra

---
+++ scripits.js
@@ -0,0 +1,43 @@
+const CaixaPrincipal = documento.quarySelector(".Caixa-Principal");
+const CaixaPregunta = documento.quarySelector(".Caixa-Pregunta");
+const CaixaAlternativas = documento.quarySelector(".Caixa-Altermarivas");
+const CaixaResultados = documento.quarySelector(".caixa-Resultados");
+const textoResultados = documento.quarySelector(".texto-Resultados");
+
+const Pregunta = [
+    {
+        enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1"
+            "alternativa 2"
+        ]
+    }
+    {
+         enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1"
+            "alternativa 2"
+        ]
+    }
+    {
+         enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1"
+            "alternativa 2"
+        ]
+    }
+    {
+         enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1"
+            "alternativa 2"
+        ]
+    }
+    {
+         enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1"
+            "alternativa 2"
+        ]
+    }
+]
